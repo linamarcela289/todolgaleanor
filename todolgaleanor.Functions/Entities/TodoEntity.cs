40f2c825ca4e9216b0268040d4817dd1418c815e
LINA MARCELA GALEANO RAMIREZ <linagaleano74784@correo.itm.edu.co>
2021-08-18 08:50:39 -0500

    Add Entities

---
+++ todolgaleanor.Functions/Entities/TodoEntity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace todolgaleanor.Functions.Entities
+{
+    public class TodoEntity : TableEntity
+    {
+        public DateTime CreatedTime { get; set; }
+
+        public string TaskDescription { get; set; }
+
+        public bool IsCompleted { get; set; }
+
+    }
+}
