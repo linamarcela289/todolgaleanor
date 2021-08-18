644949d51e019bf8a3691a4814b774b9bf2da69c
LINA MARCELA GALEANO RAMIREZ <linagaleano74784@correo.itm.edu.co>
2021-08-18 13:58:36 -0500

    Function

--- todolgaleanor.Functions/Function/TodoApi.cs
+++ todolgaleanor.Functions/Function/TodoApi.cs
@@ -20,7 +20,6 @@ namespace todolgaleanor.Functions.Function
 
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
-
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
